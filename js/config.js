ea553a3017009bab2f452eb419a32e1d723f0e21
ujumayev007-ux <u.jumayev007@gmail.com>
2026-09-12 09:34:39 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,4 @@
-
+// Serveringizning online manzili (Render, Railway yoki boshqa hosting)
+// Masalan: 'wss://oshxonachi-server.onrender.com'
+const SERVER_URL = 'wss://oshxonachi-server.onrender.com';
+const API_URL = 'https://oshxonachi-server.onrender.com';

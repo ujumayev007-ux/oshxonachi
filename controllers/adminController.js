5a7cb67769b4a8ac0c91c519a7bda9b162aae4b1
ujumayev007-ux <u.jumayev007@gmail.com>
2026-09-14 16:47:50 +0500

    Add inventory management functions to adminController

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,1 +1,10 @@
+// controllers/adminController.js
+// Omborxona va tannarx boshqaruvi uchun biznes-mantiq
+exports.getInventory = (req, res) => {
+    res.json({ success: true, message: 'Omborxona mahsulotlari ro\'yxati' });
+};
 
+exports.addInventoryItem = (req, res) => {
+    const itemData = req.body;
+    res.json({ success: true, message: 'Mahsulot omborga qo\'shildi', data: itemData });
+};

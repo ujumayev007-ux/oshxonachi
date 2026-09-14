5d5fa4bdb5f7cb79329a59cc3a877e170c4efd23
ujumayev007-ux <u.jumayev007@gmail.com>
2026-09-14 16:50:55 +0500

    Add order route to waiter router

--- routes/waiter.router.js
+++ routes/waiter.router.js
@@ -1,1 +1,8 @@
+// routes/waiter.routes.js
+const express = require('express');
+const router = express.Router();
+const waiterController = require('../controllers/waiter.controller');
 
+router.post('/order', waiterController.createOrder);
+
+module.exports = router;
